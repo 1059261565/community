5db4e99d8f76f60f8b94d0025013cd8412ab0ae1
unknown <1059261565@qq.com>
2020-06-05 16:45:04 +0800

    addindexBS

--- src/main/java/com/weikai/community/controller/IndexController.java
+++ src/main/java/com/weikai/community/controller/IndexController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class IndexController {
 
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(name="name",required = false,defaultValue ="World")String name, Model model){
-        model.addAttribute("name",name);
-        return "greeting";
+    public String greeting(){
+        return "index";
     }
 }
